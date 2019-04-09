89634f3abbee314b56612446656ee58adf3bcfca
dandenney <dd.subscriptions@gmail.com>
2019-04-09 07:47:42 -0400

    feat(blips): change the config to pages and add a single blip

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages/posts`,
+        path: `${__dirname}/src/pages/`,
         name: "posts",
       },
     },

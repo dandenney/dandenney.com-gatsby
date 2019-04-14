6b9d09f51769854f0609a68767828842e24db6ad
dandenney <dd.subscriptions@gmail.com>
2019-04-13 21:18:58 -0400

    chore(prettier): update the prettier settings and add to vscode
    closes: #11

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,33 @@
-import { Link } from "gatsby"
+import { Link } from 'gatsby';
 
 // this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
 /** @jsx jsx */
-import { css, jsx } from '@emotion/core'
+import { css, jsx } from '@emotion/core';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-const color = 'white'
+const color = 'white';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div
       css={css`
-      padding: 32px;
-      background-color: hotpink;
-      font-size: 24px;
-      border-radius: 4px;
-      &:hover {
-        color: ${color};
-      }
-    `}
+        padding: 32px;
+        background-color: hotpink;
+        font-size: 24px;
+        border-radius: 4px;
+        &:hover {
+          color: ${color};
+        }
+      `}
     >
       Hover to change color.
     </div>
 
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;

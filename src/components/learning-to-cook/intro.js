fc884e63561a9f6affafe1849a1ec780cac16133
dandenney <dd.subscriptions@gmail.com>
2019-05-04 15:55:45 -0400

    feat(learning-to-cook): test intro styles

--- src/components/learning-to-cook/intro.js
+++ src/components/learning-to-cook/intro.js
@@ -4,18 +4,16 @@ import { Header } from 'components/learning-to-cook/introStyles';
 
 const Intro = () => (
   <Header>
-    <h1>Learning to Cook</h1>
+    <h1>
+      Learning <div>to Cook</div>
+    </h1>
     <p>
       One of my favorite parts of learning is sharing. As I've been working
       towards becoming a culinarian, I've noticed that food preparation has a
-      lot of similarities with web development: you learn the techniques of the
-      craft to create something, there's much to learn from your predecessors,
-      and the goal is to share your creation. In both, some of my creations are
-      awesome, and some suck, but I'm learning and that's what keeps me happy.{' '}
-    </p>
-    <p>
-      Here I'll share journal-type posts of small experiments, techniques, etc.
-      and longer-form recipes that I've found somewhere and made my own.
+      lot of similarities with web development: there's much to learn from your
+      predecessors, and the goal is to share your creation. In both, some of my
+      creations are awesome, and some suck, but I'm learning and that's what
+      keeps me happy.
     </p>
   </Header>
 );

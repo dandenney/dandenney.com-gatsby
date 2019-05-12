f0a9e9145a6957a4c68379d1c56060960ea58929
dandenney <dd.subscriptions@gmail.com>
2019-05-12 13:56:42 -0400

    fix(home): change to home

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Layout from 'components/layout';
 import SEO from 'components/seo';
 
-import Intro from 'components/Home/intro';
+import Intro from 'components/home/intro';
 
 const IndexPage = () => (
   <Layout>

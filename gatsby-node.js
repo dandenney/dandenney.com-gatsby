2c1ba6a490399c0dfac1de1705352e7007c71c28
Dan Denney <dandenney@Dans-MacBook-Pro.local>
2019-08-27 23:06:48 -0400

    chore(dependencies): update dependencies to start the app

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,14 @@
 const path = require('path');
 
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+  });
+};
+
 exports.createPages = ({ actions, graphql }) => {
-  exports.onCreateWebpackConfig = ({ stage, actions }) => {
-    actions.setWebpackConfig({
-      resolve: {
-        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-      },
-    });
-  };
 
   const { createPage } = actions;
 

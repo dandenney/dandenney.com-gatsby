fafc92c22b550e15e4f657266cbd0b540e98b82c
Dan Denney <dandenney@Dans-MacBook-Pro.local>
2019-08-27 23:38:23 -0400

    feat(styles): add theme-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-svg`,
+    `gatsby-plugin-theme-ui`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {

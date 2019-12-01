053933a7238634b290b27313537d23fe0677415f
Dan Denney <dandenney@Dans-MacBook-Pro.local>
2019-11-30 23:45:19 -0500

    feat(theme): change the action color

--- src/patterns.js
+++ src/patterns.js
@@ -39,34 +39,6 @@ const ListNav = styled.ul`
   li:not(:first-child) {
     margin-top: 1rem;
   }
-
-  a {
-    background-image: linear-gradient(
-        to right,
-        #b51f24,
-        #b51f24 50%,
-        transparent 50%,
-        transparent 10%
-      ),
-      linear-gradient(
-        to right,
-        #b51f24,
-        #b51f24 50%,
-        transparent 50%,
-        transparent 100%
-      );
-    background-position: 0 100%, 100% 100%;
-    background-repeat: repeat-x;
-    background-size: 4px 0.055555556em, 200% 0.055555556em;
-    color: ${props => props.theme.colors.action};
-    display: inline-block;
-    text-decoration: none;
-    transition: background-position 0.3s;
-
-    &:hover {
-      background-position: 0 100%, 0 100%;
-    }
-  }
 `;
 
 export { ImgListNav, ListNav };

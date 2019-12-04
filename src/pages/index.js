cf6e0013a2ef4a90e8f51c960b274235b57f9416
Dan Denney <dandenney@Dans-MacBook-Pro.local>
2019-12-03 23:19:24 -0500

    feat(tcr): add a non-responsive draft

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Cooking from 'components/cooking';
 import Layout from 'components/layout';
 import FrontEnd from 'components/front-end';
 import SEO from 'components/seo';
+import TrueCrimeResearchers from 'components/true-crime-researchers';
 
 const IndexPage = () => (
   <Layout>
@@ -16,6 +17,7 @@ const IndexPage = () => (
     >
       <FrontEnd />
       <Cooking />
+      <TrueCrimeResearchers />
     </main>
   </Layout>
 );
